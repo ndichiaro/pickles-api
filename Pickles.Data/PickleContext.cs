e0e59f43055661e7df1218c57fa218f165435eeb
Nicholas DiChiaro <ndichiaro@gmail.com>
2018-08-19 23:16:32 -0400

    add models to dbset in context

--- Pickles.Data/PickleContext.cs
+++ Pickles.Data/PickleContext.cs
@@ -1,8 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Pickles.Data.Models;
 
 namespace Pickles.Data
 {
     public class PickleContext : DbContext
     {
+        public PickleContext(DbContextOptions<PickleContext> options)
+            : base(options)
+        { }
+
+        DbSet<PickleType> PickleTypes { get; set; }
+        DbSet<Vote> Votes { get; set; }
+        DbSet<Voter> Voters { get; set; }
     }
 }
