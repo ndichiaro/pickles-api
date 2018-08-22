8387c26ea373d21ae5427d14854f5cba92a104e9
Nicholas DiChiaro <ndichiaro@gmail.com>
2018-08-21 22:23:00 -0400

    ef migration for pickle type seed data

---
+++ Pickles.Data/Migrations/20180822021949_AddPickleTypeSeed.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pickles.Data.Migrations
+{
+    public partial class AddPickleTypeSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "PickleTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Dill" },
+                    { 2, "Sweet" },
+                    { 3, "Bread and Butter" },
+                    { 4, "Sour" },
+                    { 5, "Other" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PickleTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PickleTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "PickleTypes",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "PickleTypes",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "PickleTypes",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
--- Pickles.Data/PickleContext.cs
+++ Pickles.Data/PickleContext.cs
@@ -15,6 +15,13 @@ namespace Pickles.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<PickleType>().HasData(
+                new PickleType { Id = 1, Name = "Dill" },
+                new PickleType { Id = 2, Name = "Sweet" },
+                new PickleType { Id = 3, Name = "Bread and Butter" },
+                new PickleType { Id = 4, Name = "Sour" },
+                new PickleType { Id = 5, Name = "Other" }
+            );
         }
     }
 }
