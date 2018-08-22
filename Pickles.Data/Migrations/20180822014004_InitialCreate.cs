25c361226750531e0e0836c57558c8d668526112
Nicholas DiChiaro <ndichiaro@gmail.com>
2018-08-21 22:07:11 -0400

    add initial create migration

---
+++ Pickles.Data/Migrations/20180822014004_InitialCreate.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pickles.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PickleTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PickleTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Voters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Latitute = table.Column<double>(nullable: false),
+                    Longitude = table.Column<double>(nullable: false),
+                    IpAddress = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Voters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Votes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    PickleTypeId = table.Column<int>(nullable: false),
+                    VoterId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Votes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Votes_PickleTypes_PickleTypeId",
+                        column: x => x.PickleTypeId,
+                        principalTable: "PickleTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Votes_Voters_VoterId",
+                        column: x => x.VoterId,
+                        principalTable: "Voters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Votes_PickleTypeId",
+                table: "Votes",
+                column: "PickleTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Votes_VoterId",
+                table: "Votes",
+                column: "VoterId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Votes");
+
+            migrationBuilder.DropTable(
+                name: "PickleTypes");
+
+            migrationBuilder.DropTable(
+                name: "Voters");
+        }
+    }
+}
---
+++ Pickles.Data/PicklesContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Pickles.Data
+{
+    public class PicklesContextFactory : IDesignTimeDbContextFactory<PickleContext>
+    {
+        public PickleContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<PickleContext>();
+            builder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=PickleDatabase;Trusted_Connection=True;");
+            return new PickleContext(builder.Options);
+        }
+    }
+}
